94a762f32e5eeb63592e3d2a8ebca52a703a539b
MDY <mikael.dautrey@isitix.com>
2017-05-15 15:40:23 +0200

    test

--- src/Match.php
+++ src/Match.php
@@ -7,16 +7,29 @@ final class Match{
     private $score1;
     private $score2;
     
-    public __construct(string $equipe1, string $equipe2, int $score1, 
-    int $score2, date $dateMatch){
+    public __construct($equipe1, $equipe2, $score1,
+                        $score2, $dateMatch){
+        checkEquipe($equipe1);
+        checkEquipe($equipe2);
       $this->equipe1 = $equipe1;
       $this->equipe2 = $equipe2;
-      
+      $this->score1 = $score1;
+      $this->score2 = $score2;
+      $this->dateMatch = $dateMatch;
       
   
     }
-    private static function isEquipe1(string $equipe1){
-     // $regexp = /^[A-Z][A-Za-z0-9]{,40}$/;
+    private static function checkEquipe(string $equipe): void{
+        $regexp = '/^[A-Z][A-Za-z0-9]{,40}$/';
+        if(!is_string($equipe) || strlen($equipe)>41 || !preg_match('/^[A-Z][A-Za-z0-9]{,40}$/;',$equipe){
+            if (!filter_var($equipe, FILTER_VALIDATE_REGEXP, 
+                array("options"=>array("regexp"=> $regexp)))) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    '"%s" n est pas une equipe valide',
+                    $equipe
+                )
+            );
       
     }
     
