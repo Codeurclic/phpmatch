a369b69fed3381fd4bcbbce9a666d362f25197e5
MDY <mikael.dautrey@isitix.com>
2017-05-15 15:11:56 +0200

    tests ameliores

--- src/Match.php
+++ src/Match.php
@@ -2,4 +2,23 @@
 declare(strict_types=1);
 final class Match{
     private $equipe1;
+    private $equipe2;
+    private $dateMatch;
+    private $score1;
+    private $score2;
+    
+    public __construct(string $equipe1, string $equipe2, int $score1, 
+    int $score2, date $dateMatch){
+      $this->equipe1 = $equipe1;
+      $this->equipe2 = $equipe2;
+      
+      
+  
+    }
+    private static function isEquipe1(string $equipe1){
+     // $regexp = /^[A-Z][A-Za-z0-9]{,40}$/;
+      
+    }
+    
 }
+
--- test/MatchTest.php
+++ test/MatchTest.php
@@ -4,6 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 
 final class MatchTest extends TestCase {
+    // verifier la presence des "bons" attributs dans l objet
     public function testHasEquipe1(){
         $this->assertObjectHasAttribute('equipe1', new Match);
     }
@@ -15,4 +16,28 @@ final class MatchTest extends TestCase {
     public function testHasDateMatch(){
         $this->assertObjectHasAttribute('dateMatch', new Match);
     }
+    // factoriser la verification des attributs
+    public function testHasAttributes(){
+      $attributes = array('equipe1', 'equipe2', 'score1', 'score2', 'dateMatch');
+      foreach($attributes as $attribute){
+	$this->assertObjectHasAttribute($attribute, new Match);
+      }
+    }
+        
+    // verifier que la construction de l objet avec de "mauvais" parametres leve une exception
+    public function testCannotBeCreatedFromNegativeScore(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $match = new Match('Bleu', 'Rouge', -1, 0, '12/12/2012');
+    }
+    
+    // verifier le comportement de ma methode matchLocation
+    // 3 cas a tester
+    // si score1 > score2 => domicile
+    // si score1 < score2 => invité
+    // si score1 == score2 => ''
+    
+    
+    
 }
